45311045ad7a4c50f1516100d4953f6c25614795
Michael <mickaellunzola@gmail.com>
2023-07-27 12:07:03 +0100

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-//ugvgyvg
+const aleatorMessage = () =>{
+    let msgs = ['Bonjour', 'Vous allez bien', 'je suis disponible', 'Pour le rendez-vous'];
+
+    let msg = msgs[Math.floor(Math.random() * msgs.length)];
+
+    return msg
+
+}
